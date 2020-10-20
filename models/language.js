12b88b0a985d897ab64b80565e9fc1bcc13ebc8a
Thomas Adams <tqsare@gmail.com>
2020-10-19 22:43:30 -0700

    finish language routes

--- models/language.js
+++ models/language.js
@@ -1,4 +1,5 @@
 const db = require("../db");
+const sqlForPartialUpdate = require("../helpers/partialUpdate");
 const partialUpdate = require("../helpers/partialUpdate");
 
 class Language {  
@@ -15,8 +16,56 @@ class Language {
         const result = await db.query(`SELECT * FROM languages WHERE
             lang_name = $1
           `, [lang_name])
+          return result.rows[0]
       }
 
+      static async update(name, data){
+        let {query, values} = sqlForPartialUpdate('languages', data,
+         'lang_name', name);
+
+        const result = await db.query(query, values);
+        const language = result.rows[0]
+
+        return language
+      }
+
+      static async create(lang) {
+        const duplicateCheck = await db.query(`
+          SELECT lang_name FROM languages WHERE lang_name = $1
+        `, [lang.lang_name]);
+
+        if (duplicateCheck.rows[0]){
+          let duplicateError = new Error(
+            `There already exists a language with name '${lang.lang_name}`);
+            duplicateError.status = 409; // 409 Conflict
+            throw duplicateError
+        }
+
+        const result = await db.query(
+          `
+          INSERT INTO languages
+          (lang_name, lang_code, docs, website, logo_url) 
+          VALUES ($1, $2, $3, $4, $5) 
+          RETURNING lang_name, lang_code, docs, website, logo_url
+          `, 
+          [lang.lang_name, lang.lang_code, lang.docs, lang.website, lang.logo_url]
+        )
+        return result.rows[0]
+      }
+
+      static async delete(lang) {
+        const result = await db.query(
+          `DELETE FROM languages
+          WHERE lang_name = $1`, 
+          [lang]
+        );
+        // if(result.rows.length === 0){
+        //   let notFound = new Error(`There exists no language ${lang}`);
+        //   notFound.status = 404
+        //   throw notFound;
+        // }
+
+      }
 }
 
 module.exports = Language;
--- routes/languages.js
+++ routes/languages.js
@@ -22,8 +22,46 @@ router.get('/', async (req, res, next) =>{
     }
 })
 // Get one lang
+router.get('/:lang_name', async (req,res, next) => {
+    let lang_name = req.params.lang_name
+    try {
+        let lang = await Language.getLanguage(lang_name)
+        return res.json({lang})
+    } catch (error) {
+        return next(error)
+    }
+})
+
 // Edit Lang detail
+router.patch('/:lang_name', async(req, res, next) => {
+    try {
+        let lang = await Language.update(req.params.lang_name, req.body)
+        return res.json({lang})
+    } catch (error) {
+        return next(error)
+    }
+    
+})
+
 // Create Lang if admin
+router.post('/', async(req, res, next) => {
+    try {
+        const lang = await Language.create(req.body)
+        return res.status(201).json({lang})
+    } catch (error) {
+        return next(error)
+    }
+})
+
 // Delete lang if admin
+router.delete('/:lang_name', async(req, res, next) => {
+    console.log(req.params.lang_name);
+    try {
+        await Language.delete(req.params.lang_name)
+        return res.json({message: 'Language Deleted'})
+    } catch (error) {
+        return next(error)
+    }
+})
 
 module.exports = router
